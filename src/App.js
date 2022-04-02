e5f453e9d380a67954cef5583a9e6a552209f2bd
ndianekwu sarah <sarahndianekwu@gmail.com>
2022-04-02 02:30:25 -0700

    just finished building my header page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './component/header/header.component';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './page/homepage/homepage.component';
 import ShopPage from './page/shoppage/shoppage.component';
@@ -9,6 +10,7 @@ import './App.css';
 function App() {
   return (
     <div >
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route  path='/shop' element={<ShopPage />} />
