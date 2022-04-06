ba9806875c557af45af987aa99fae1b3c2f2c9de
ndianekwu sarah <sarahndianekwu@gmail.com>
2022-04-06 07:54:39 -0700

    add reselect to reduce the redering of other component

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CustomButton from '../custom-button/custom-button.component';
 import { connect } from "react-redux";
+import { selectCartItems } from "../cart/cart.selector";
 import CartItem from "../cart-item/cart-item.component";
 import './cart-dropdown.style.scss';
 
@@ -17,8 +18,8 @@ const CartDropdown = ({ cartItems }) => (
     </div>
 );
 
-const mapStateToProps = ({cart: { cartItems }}) =>({
-    cartItems
+const mapStateToProps = state =>({
+    cartItems: selectCartItems(state)
 });
 
 export  default connect(mapStateToProps)(CartDropdown);
